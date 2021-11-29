49fb459ce9cde90652a24225a566a1a5474e0bb2
solenedel <solene98@student.ubc.ca>
2021-11-29 12:04:32 -0800

    style home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,7 +4,21 @@ import React from 'react';
 export const HomePage = ({ className }) => {
   return (
     <main className={className} id="home-page-container">
-      <div>Home Page</div>
+      <div className="today list">
+        <h2>Today</h2>
+        <ul>
+          <li>clean my room</li>
+          <li>wash the dishes</li>
+          <li>reply to emails</li>
+        </ul>
+      </div>
+      <div className="tomorrow list">
+        <h2>Tomorrow</h2>
+        <ul>
+          <li>exercise</li>
+          <li>buy groceries</li>
+        </ul>
+      </div>
     </main>
   );
 };
--- src/components/styled-components/HomePage.style.js
+++ src/components/styled-components/HomePage.style.js
@@ -1,68 +1,41 @@
 /* eslint-disable import/prefer-default-export */
 import styled from 'styled-components';
 import { HomePage } from '../HomePage';
-import { colorVars, fontVars, mainButtonStyles, inputFieldStyles } from './css-variables';
+import { colorVars, fontVars } from './css-variables';
 
 export const StyledHomePage = styled(HomePage)`
-  border: solid blue 3px;
+  /* border: solid blue 3px; */
   /* height: 100vh; */
-  height: 500px;
   /* width: fit-content; */
+  height: fit-content;
   width: 100vw;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  flex-wrap: wrap;
   align-items: center;
+  justify-content: center;
+  font-size: 20px;
 
-  & img {
-    width: 100px;
-    display: none;
+  .list {
+    margin: 20px 50px 0 50px;
   }
 
-  & h3 {
-    margin: 0;
-    padding: 0;
-    position: absolute;
-    top: 70%;
-    left: 13%;
-    z-index: 10;
-    font-size: 25px;
-    color: ${colorVars.primaryDark};
-  }
-
-  & div {
-    font-size: 25px;
-    margin-bottom: 30px;
-    color: ${colorVars.primaryDark};
-  }
-
-  & #search-section {
+  & h2 {
     font-family: ${fontVars.mainFont};
-    margin: 50px 0 20px 0;
+    margin-bottom: 12px;
+    font-weight: 400;
+  }
 
-    & input {
-      @include ${inputFieldStyles};
-      height: 30px;
+  ul {
+    border: solid ${colorVars.primaryGreen} 3px;
+    background: ${colorVars.primaryGreen};
+    border-radius: 6px;
+    padding: 10px;
 
-      &:hover {
-        background-color: ${colorVars.mediumPink};
-        transition: background-color 0.4s;
-      }
-
-      &::placeholder {
-        color: ${colorVars.primaryDark};
-        opacity: 0.6;
-        padding-left: 5px;
-      }
-    }
-
-    & button {
-      margin-left: 20px;
-      @include ${mainButtonStyles};
-
-      &:hover {
-        color: ${colorVars.linkHover};
-        transition: color 0.4s;
-      }
+    & li {
+      margin-bottom: 25px;
+      min-width: 200px;
+      color: black;
     }
   }
 `;
