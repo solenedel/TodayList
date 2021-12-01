bd98e085a9a5f84e01518dcbf9b72d2c41c384f9
solenedel <solene98@student.ubc.ca>
2021-11-30 16:58:21 -0800

    style Todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,9 +3,16 @@ import React from 'react';
 
 const Todo = ({ todo }) => {
   return (
-    <div>
+    <div style={{ display: 'flex' }}>
       <input type="checkbox" />
-      <li>{todo.task}</li>
+      <li
+        style={{
+          color: 'white',
+          textDecoration: todo.completed ? 'line-through' : null,
+        }}
+      >
+        {todo.task}
+      </li>
       <button type="button">X</button>
     </div>
   );
